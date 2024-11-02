39c55781177f0ba8e597600b740404232d10153e
Kelvin Adithya <72134234+kelvinkn17@users.noreply.github.com>
2024-11-03 06:44:49 +0700

    feat: update contract

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { userService } from "./app/services/user.js";
 
-async function run() {
+async function generateQR() {
   const blob = await userService.generateQrCode(
     "https://bozo.squidl.me",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfcoTwHrfGAUBiFJv5FedKRwdXpd8mkStN-Q&s"
@@ -9,5 +9,3 @@ async function run() {
   fs.writeFileSync("qr-code.png", blob);
   console.log({ blob });
 }
-
-run();
